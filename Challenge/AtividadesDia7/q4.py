69db13b4740abb122d64acfcac17d8945a09d23e
kitsune <carlosufpa10@gmail.com>
2022-06-08 17:25:08 -0300

    Update q4.py
    Alterei: nome da variavel dict_Visitante, para visitante_manipulavel, para seguir o padrão das outras questões

--- Challenge/AtividadesDia7/q4.py
+++ Challenge/AtividadesDia7/q4.py
@@ -1,8 +1,8 @@
 import json
 def Q4():
     with open("./Challenge/AtividadesDia7/Dados/json1.json") as time_Visitante:
-        dict_Visitante = json.load(time_Visitante)
-        return dict_Visitante
+        visitante_manipulavel = json.load(time_Visitante)
+        return visitante_manipulavel
 visitante_retornado = Q4()
 copa = visitante_retornado["copa-do-brasil"][0]
 

66e6ad6932e4b9c118834a4172a2678d16c8615a
kitsune <carlosufpa10@gmail.com>
2022-06-08 23:18:01 -0300

    Update q5.py

--- Challenge/AtividadesDia7/q5.py
+++ Challenge/AtividadesDia7/q5.py
@@ -1,7 +1,8 @@
 import json
-def Q4():
+def Q5():
     with open("./Challenge/AtividadesDia7/Dados/json2.json") as campeonato :
         campeonato_manipulavel= json.load(campeonato)
         return campeonato_manipulavel
-campeonatoRetornado= Q4()
+campeonatoRetornado= Q5()
+
 print(campeonatoRetornado)

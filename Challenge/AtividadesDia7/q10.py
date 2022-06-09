9f69db891754fdb2deb5f9673bc5663e45f877e1
kitsune <carlosufpa10@gmail.com>
2022-06-08 23:24:35 -0300

    Update q10.py

--- Challenge/AtividadesDia7/q10.py
+++ Challenge/AtividadesDia7/q10.py
@@ -1,3 +1,4 @@
+from email.mime import base
 import pandas as pd
 baseDeDados = pd.read_csv("./Challenge/AtividadesDia7/Dados/csv.csv")
-print(baseDeDados.loc[10,["Movie"]])
+print(baseDeDados.loc[74,["Year"]])
